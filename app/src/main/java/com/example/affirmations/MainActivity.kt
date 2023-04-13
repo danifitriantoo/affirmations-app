b11f9863dc4b99060a8e9350323e842750e4c624
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-04-13 16:24:17 +0700

    recyclerview using kotlin

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvAffirmation: TextView = findViewById(R.id.tv_affirmation)
+        tvAffirmation.text = DataSource().loadAffirmations().size.toString()
     }
 }
